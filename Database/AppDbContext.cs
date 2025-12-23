4ffae02e30583dfd32a834d44b759316445ce7ad
akhilbju <abiju60@gmail.com>
2025-12-23 19:06:27 +0530

    Add RefreshToken model and authentication endpoints; configure Swagger and database migrations

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -8,5 +8,20 @@ public class AppDbContext : DbContext
     }
 
     public DbSet<User> Users { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+                    .Property(u => u.Id)
+                    .ValueGeneratedOnAdd();
+                    
+        modelBuilder.Entity<RefreshToken>(entity =>
+        {
+            entity.HasKey(e => e.TokenId);
+            entity.Property(e => e.TokenId).ValueGeneratedOnAdd();
+        });
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
@@ -12,6 +15,7 @@ builder.Services.Configure<JwtConfigurationModel>(
     builder.Configuration.GetSection("Jwt"));
 
 builder.Services.AddScoped<IJwtSettings, JwtSettings>();
+builder.Services.AddScoped<IPasswordHasherService, PasswordHasherService>();
 
 var jwtConfig = builder.Configuration.GetSection("Jwt").Get<JwtConfigurationModel>()!;
 
@@ -38,10 +42,16 @@ builder.Services.AddAuthorization();
 builder.Services.AddOpenApi();
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
 app.UseAuthentication();
